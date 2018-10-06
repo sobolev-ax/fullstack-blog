47242f773fa0b90a45bb3da58939cdd25c9fa134
sobolev.ax <sobolev.ax@gmail.com>
2018-10-06 20:56:37 +0300

    added example mongodb as keys.js

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb://<adminuser>:<password>@ds012345-a0.mlab.com:56789/fullstack-blog'
+}
+
+// To connect using a driver via the standard MongoDB URI
+// https://mlab.com/

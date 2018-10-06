20db20ee6fe7f1cc2bde11a9cdb796f0fcdac9ca
sobolev.ax <sobolev.ax@gmail.com>
2018-10-06 20:19:19 +0300

    add app.use bodyParser logic in app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongooseConnectParams = {
     useNewUrlParser: true
 };
 const postRouter = require('./routes/post');
+const bodyParser = require('body-parser');
 
 mongoose.connect(keys.mongoURI, mongooseConnectParams)
     .then(() => { console.log('MongoDB conected')})
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000;
 const clientPath = path.join(__dirname, 'client');
 
 const app = express();
+app.use(bodyParser.json());
 app.use('/api/post', postRouter);
 app.use(express.static(clientPath));
 

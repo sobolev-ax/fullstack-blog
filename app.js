a31815177307cb0fc95a295f8d516e6496392db8
sobolev.ax <sobolev.ax@gmail.com>
2018-10-06 13:24:44 +0300

    added first realization of server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+const clientPath = path.join(__dirname, 'client');
+
+const app = express();
+app.use(express.static(clientPath));
+
+app.listen(port, () => {
+    console.log(`Server has been started on port ${port}`);
+});

4f448e4f15c7677d89b0df97c4b03af97582da97
sobolev.ax <sobolev.ax@gmail.com>
2018-10-06 14:14:23 +0300

    added database connection in app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const keys = require('./keys');
+const mongooseConnectParams = {
+    useNewUrlParser: true
+};
+
+mongoose.connect(keys.mongoURI, mongooseConnectParams)
+    .then(() => { console.log('MongoDB conected')})
+    .catch(error => { console.error(error)})
 
 const port = process.env.PORT || 5000;
 const clientPath = path.join(__dirname, 'client');
